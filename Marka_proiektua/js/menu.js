a5c965e5914d62498ee16e4fad8dfec37426fdae
kaietba <118171370+kaietba@users.noreply.github.com>
2023-04-18 14:21:26 +0200

    Add files via upload

---
+++ Marka_proiektua/js/menu.js
@@ -0,0 +1,32 @@
+class menu extends HTMLElements {
+	constructor(){
+		super();
+		this.innerHTML= 
+    <nav class="navbar navbar-expand-md navbar-light bg-light">
+      <div class="container-fluid">
+        <a class="navbar-brand href" href="#"><img src="argazkiak/logoa-2.png" width="125" height="100"></a>
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="offset-8 collapse navbar-collapse" id="navbarSupportedContent">
+          <ul class="navbar-nav me-auto mb-2 mb-lg-0 align-items-right">
+            <li class="nav-item">
+              <a class="nav-link active" aria-current="page" href="index.html">Hasiera</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="helmugak.html">Helmugak</a>
+            </li>
+          
+            <li class="nav-item">
+              <a class="nav-link active" href="eztantziak.html" tabindex="-1" aria-disabled="true">Eztantziak</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="kontaktua.html">Kontaktua</a>
+            </li>
+          </ul>
+          </div>
+       </div>
+      </nav>
+	}
+}
+customElements.define('menu-component', Menu);
