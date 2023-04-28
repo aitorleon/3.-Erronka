e4348d6d339155d951e52d1dd53f4217f693480d
kaietba <118171370+kaietba@users.noreply.github.com>
2023-04-28 14:04:49 +0200

    Add files via upload

---
+++ Marka_proiektua/js/footer.js
@@ -0,0 +1,25 @@
+FooterString = 
+'<div class="container-fluid">' +
+    '<div class="row p-5 bg-light text-center">'+
+   
+        '<div class="iconos col-md-6 col-lg-4 ">'+
+          '<h3>Gure Sare Sozialak</h3>'+
+          '<!--<a class="icono-twitter" href="https://twitter.com"><i class="bi bi-twitter"></i></a>-->'+
+          '<a href="https://twitter.com"><img width="30px" height="30x" src="argazkiak/twitter.png"></a>'+ 
+          '<!--<a class="icono-mail" href="https://mail.google.com/mail/u/0/?tab=rm&ogbl#inbox"><i class="bi bi-google"></i></a>-->'+
+          '<a href="https://youtube.com"><img width="25px" height="20px" src="argazkiak/mail.png"></a>'+
+          '<!--<a class="icono-instagram" href="https://instagram.com"><i class="bi bi-instagram "></i></a>-->'+
+          '<a href="https://instagram.com"><img width="20px" height="20px" src="argazkiak/instagram.png"></a>'+
+          '<!--<a class="icono-facebook" href="https://facebook.com"><i class="bi bi-facebook"></i></a>-->'+
+          '<a href="https://facebook.com"><img width="20px" height="20px" src="argazkiak/facebook.png"></a>'+
+        '</div>'+
+         
+        '<div class="col-md-6 col-lg-4">'+
+           '<h3>Kontaktua</h3>'+
+           '<p><i class="bi bi-telephone"> +34 612 23 42 51</i></p>'+
+            '<p><i class="bi bi-envelope"> ikabidaiak@gmail.com</i></p>'+
+        '</div>'+
+        '<div class="col-md-12 col-lg-4"><img src="argazkiak/logoa-2.png" width="145" height="120"></div>'+
+    '</div>'+
+  '</div>';
+  document.write(FooterString);
